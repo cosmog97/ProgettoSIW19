3c3f47368f18455e7572b2020825e5c096af13b3
cosmo <cosmo@160.97.143.97>
2019-02-06 17:13:44 +0100

    Login && Register 2

---
+++ ProgettoSIW/WebContent/js/validateform.js
@@ -0,0 +1,10 @@
+function myFunction(p1, p2){
+var email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I expect an e-mail, darling!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
